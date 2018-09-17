2a2cacd596890d433e728f5490866bc8412df2e9
Estudiante <Estudiante@10.203.1.139>
2018-09-17 09:05:58 -0500

    Carros

---
+++ src/carros/Carros.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carros;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Carros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            //Nombre, Apellido y Cantidad de autos
+            
+            Ruedas[] rueda = new Ruedas[4];
+            for(int i=0;i<4;i++){
+            rueda[1]= new Ruedas("400");
+            }
+            Persona persona1= new Persona("Diego", "Lopez", 8);
+            Carro carro1 = new Carro(rueda,"chasis", "2500", "Ford");
+            
+            persona1.setCarros(carros);
+            Carro[] Car=persona1.listaCarros();
+            for (int i = 0; i<1 ;i++) {
+                System.out.println("Carro #"+i+" Chasis:"+ Car[i].getChasis().getTipo()+ " Motor:"+Car[i].getMotor()+ " Ruedas: "+Car[i].getRueda()[1].getTipo());
+            }
+        }
+            
+            
+            
+ }
+    
+
